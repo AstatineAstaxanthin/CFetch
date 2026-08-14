5cfaac71211597f1c324214d8b802be8e7dc4038
AstatineAstaxanthin <Astatine.Astaxanthin@gmail.com>
2026-08-15 00:30:22 +0900

    Implement HTTP POST request with JSON payload
    Fix command variable name and format payload for JSON.

---
+++ exampleuse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char* hook = "hook address";
+
+// JSON
+ {"content":"infos that u wanna send"}
+    char* payload = "{\"content\": \"infos that u wanna send\"}";
+
+    char cmd[2048];
+    sprintf(cmd, "node ~/httpget.js \"%s\" \"POST\" '%s'", hook, payload);
+
+    system(command);
+
+    return 0;
+}
